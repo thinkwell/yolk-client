7425f18ee61f0eba95f8470ffb5b4e1f15baa8a6
Paul Strong <paul@thestrongfamily.org>
2011-04-13 16:59:12 -0500

    Add specs for enrollment state. Add search shortcut

--- lib/yolk-client/client/enrollments.rb
+++ lib/yolk-client/client/enrollments.rb
@@ -6,8 +6,19 @@ module Yolk
 
       def enrollments *args
         options = args.last.is_a?(Hash) ? args.pop : {}
+        search = options.delete :search
+        search.each_pair{|k,v| options["search[#{k}]"] = v} if search
         response = get('enrollments', options)
-        response
+        prepare_enrollments response
+      end
+
+      private
+      def prepare_enrollments enrollments
+        enrollments.each do |e|
+          e.start_date = Time.parse(e.start_date) if e.start_date
+          e.end_date = Time.parse(e.end_date) if e.end_date
+        end
+        enrollments
       end
 
     end
--- spec/yolk-client/client/enrollments_spec.rb
+++ spec/yolk-client/client/enrollments_spec.rb
@@ -9,23 +9,32 @@ describe Yolk::Client do
     use_vcr_cassette
     context "with relevant_to_user passed" do
       it "should return only enrollments for that user" do
-        enrollments = @client.enrollments "search[relevant_to_user]" => "admin@thinkwell.com"
+        enrollments = @client.enrollments :search => {:relevant_to_user => "admin@thinkwell.com"}
         enrollments.should_not be_empty
         enrollments.all?{|e| [e.owner, e.assigned_to].should include "admin@thinkwell.com"}
       end
     end
     context "with owner_equals passed" do
       it "should return only enrollments for that owner" do
-        enrollments = @client.enrollments "search[owner_equals]" => "admin@thinkwell.com"
+        enrollments = @client.enrollments :search => {:owner_equals => "admin@thinkwell.com"}
         enrollments.should_not be_empty
         enrollments.all?{|e| e.owner.should == "admin@thinkwell.com"}
       end
     end
     context "with limit" do
       it "should only return the limit of enrollments" do
-        enrollments = @client.enrollments "search[limit_results]" => 20
+        enrollments = @client.enrollments :search => {:limit_results => 20}
         enrollments.count.should == 20
-
+      end
+    end
+    context "with state" do
+      it "should return active enrollments" do
+        enrollments = @client.enrollments :search => {:state => :active, :limit_results => 20}
+        enrollments.all?{|e| !e.assigned_to.nil? && e.end_date > Time.now}
+      end
+      it "should return expired enrollments" do
+        enrollments = @client.enrollments :search => {:state => :expired, :limit_results => 20}
+        enrollments.all?{|e| e.end_date < Time.now}
       end
     end
   end
