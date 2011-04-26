984e7109d4787101c5ddc27dcd615934e2765fc9
Paul Strong <paul@thestrongfamily.org>
2011-04-26 11:44:12 -0500

    Add method_missing shortcut so we can do Yolk.enrollments instead of Yolk.new.enrollments.

--- lib/yolk-client.rb
+++ lib/yolk-client.rb
@@ -5,7 +5,19 @@ require 'yolk-client/error'
 module Yolk
   extend Configuration
 
+  # Alias for Yolk::Client.new
   def self.client(options={})
     Yolk::Client.new(options)
+  end
+
+  # Delegate to Yolk::Client
+  def self.method_missing(method, *args, &block)
+    a_client = client
+    return super unless a_client.respond_to?(method)
+    a_client.send(method, *args, &block)
+  end
+
+  def self.respond_to?(method, include_private = false)
+    client.respond_to?(method, include_private) || super(method, include_private)
   end
 end
