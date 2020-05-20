799f25124cd5ceaba5b95511a5f227658cb6b339
Dominik Steiner <dominik.j.steiner@gmail.com>
2020-05-20 08:48:20 -0600

    thinkwell/yolk#78 : change User model to not inherit from OpenStruct and add is_dirty?

--- lib/yolk-client/models/user.rb
+++ lib/yolk-client/models/user.rb
@@ -1,12 +1,90 @@
 module Yolk
   module Models
-    class User < OpenStruct
-      def username
-        super || id || email
+    class User
+      def initialize(hash = {})
+        attributes = hash.with_indifferent_access
+        @first_name = attributes[:first_name]
+        @last_name = attributes[:last_name]
+        @display_name = attributes[:display_name]
+        @username = attributes[:username]
+        @email = attributes[:email]
+        @student_id = attributes[:student_id]
+        @id = attributes[:id]
       end
 
-      def as_json(options = nil)
-        to_h.as_json
+      def display_name
+        @display_name
+      end
+      def display_name=(val)
+        mark_dirty unless val == @display_name
+        @display_name = val
+      end
+
+      def first_name
+        @first_name
+      end
+      def first_name=(val)
+        mark_dirty unless val == @first_name
+        @first_name = val
+      end
+
+      def last_name
+        @last_name
+      end
+      def last_name=(val)
+        mark_dirty unless val == @last_name
+        @last_name = val
+      end
+
+      def username
+        @username || id || email
+      end
+      def username=(val)
+        mark_dirty unless val == @username
+        @username = val
+      end
+
+      def email
+        @email
+      end
+      def email=(val)
+        mark_dirty unless val == @email
+        @email = val
+      end
+
+      def student_id
+        @student_id
+      end
+      def student_id=(val)
+        mark_dirty unless val == @student_id
+        @student_id = val
+      end
+
+      def id
+        @id
+      end
+      def id=(val)
+        mark_dirty unless val == @id
+        @id = val
+      end
+
+      def is_dirty?
+        !!@is_dirty
+      end
+      def mark_dirty
+        @is_dirty = true
+      end
+
+      def to_h
+        {
+          :first_name => first_name,
+          :last_name => last_name,
+          :display_name => display_name,
+          :username => username,
+          :email => email,
+          :student_id => student_id,
+          :id => id
+        }
       end
     end
   end
